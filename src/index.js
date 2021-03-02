f2030d208e085a7d78ad40d1a3be350025f43994
Daniel Sasse <dsasse07@gmail.com>
2021-03-02 12:03:50 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// Step 1
+import { BrowserRouter as Router, Route, Switch, NavLink } from "react-router-dom"
 
 function Home() {
   return (
@@ -9,4 +11,85 @@ function Home() {
   );
 }
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+}
+
+/* Add basic styling for NavLinks */
+const linkStyles = {
+  width: "100px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+/* add the navbar component */
+function NavBar() {
+  return (
+    <div>
+      <NavLink
+        to="/"
+        /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+        exact
+        /* add styling to Navlink */
+        style={linkStyles}
+        /* add prop for activeStyle */
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink to="/about" exact style={linkStyles} activeStyle={{ background: "darkblue"}}>
+        About
+      </NavLink>
+      <NavLink to="/login" exact style={linkStyles} activeStyle={{ background: "darkblue" }}>
+        Login
+      </NavLink>
+    </div>
+  );
+}
+
+// When the route matches the "/" path the components in that route will be rendered
+
+ReactDOM.render(
+  <Router>
+    <NavBar/>
+    <Switch>
+      <Route exact path="/about"> 
+        <About />
+      </Route>
+      <Route exact path="/login"> 
+        <Login />
+      </Route>
+      <Route path="/"> 
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
+
+, document.getElementById("root"));
